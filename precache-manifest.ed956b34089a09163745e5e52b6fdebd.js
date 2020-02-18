e14237cb9b1e62591e91f7b69078ac79d0122a3f
ali atik <ali1wg@gmail.com>
2020-02-18 20:39:44 +0330

    Updates

---
+++ precache-manifest.ed956b34089a09163745e5e52b6fdebd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a67aefe45d53190a1eb3eb851a9a10b8",
+    "url": "/lynq/index.html"
+  },
+  {
+    "revision": "c996331bcee90ceee15a",
+    "url": "/lynq/static/css/main.55420d70.chunk.css"
+  },
+  {
+    "revision": "b726229c699c78619232",
+    "url": "/lynq/static/js/2.8987d177.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/lynq/static/js/2.8987d177.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c996331bcee90ceee15a",
+    "url": "/lynq/static/js/main.eeaa96ba.chunk.js"
+  },
+  {
+    "revision": "473a314bef468eb6134c",
+    "url": "/lynq/static/js/runtime-main.a7e53293.js"
+  }
+]);
